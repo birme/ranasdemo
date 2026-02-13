eb6eebbae784368ef7bef53b849b946a5e410ba3
Jonas Birmé <jonas.birme@eyevinn.se>
2026-02-13 14:39:42 +0100

    Use port 8080 as default for Web Runner

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { Pool } = require("pg");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const pool = new Pool({
   connectionString:
